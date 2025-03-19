fd77b8cef48bb702bf0c41185ef2d3b432cb4af1
DingAlessandro <121311960+DingAlessandro@users.noreply.github.com>
2025-03-19 10:15:43 +0100

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-const cartCount = document.querySelector('.cart-count');
-let count = 0;
-
-function updateCart() {
-    count++;
-    cartCount.textContent = count;
-}
-
-document.querySelector('.navbar-cart').addEventListener('click', updateCart);
